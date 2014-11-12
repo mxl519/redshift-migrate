b24e6427f8a1afaf12ba7d508803977d6a860f61
Eduardo Gonzalez <eduardog@telenav.com>
2014-11-12 15:52:18 -0800

    Drop prototype tables for deprecated monthly summaries
    https://thinkneardev.atlassian.net/browse/REX-1483
    
    Drops the deprecated prototypes instead of continuing to support them across
    new format changes.
    
    Also removes prototype tables from schema dumps.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 RedshiftMigrate::Application.initialize!
+
+# Ignore partitioned tables when dumping schema (detected by at least 4 digits at the end of the table name)
+ActiveRecord::SchemaDumper.ignore_tables = [/_\d{4,}\Z/]
