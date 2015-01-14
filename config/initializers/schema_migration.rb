43bbeb0043b3312b591d5d7d62de81f9e8459dec
Xiaoke Li <michael@thinknear.com>
2015-01-13 20:22:44 -0800

    Whoops typos
    https://thinkneardev.atlassian.net/browse/ES-1020

--- config/initializers/schema_migration.rb
+++ config/initializers/schema_migration.rb
@@ -1,8 +1,10 @@
 case ENV['RAILS_ENV']
 when /engineering$/i
   ActiveRecord::Migrator.migrations_paths = ['db/migrate/common', 'db/migrate/engineering']
+  Rails.application.paths['db/migrate'] = ['db/migrate/common', 'db/migrate/engineering']
 when /ops$/i
   ActiveRecord::Migrator.migrations_paths = ['db/migrate/common', 'db/migrate/ops']
+  Rails.application.paths['db/migrate'] = ['db/migrate/common', 'db/migrate/ops']
 else
   puts "Failed to load database settings because invalid RAILS_ENV given: #{ENV['RAILS_ENV']}"
   ActiveRecord::Migrator.migrations_paths = ['db/migrate/common']
