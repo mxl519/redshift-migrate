0d5c7cf16dba692e7046f8013a02777c685591d0
Jigesh Vora <jigeshv@thinknear.com>
2015-01-19 14:08:13 -0800

    Updated ip_path and caf_path column size to 512 characters https://thinkneardev.atlassian.net/browse/GARD-1364

--- db/migrate/common/20150119214825_update_io_caf_paths_size.rb
+++ db/migrate/common/20150119214825_update_io_caf_paths_size.rb
@@ -1,6 +1,6 @@
 class UpdateIoCafPathsSize < ActiveRecord::Migration
   def up
-  	rename_column :contracts, :io_path, :io_path_old
+    rename_column :contracts, :io_path, :io_path_old
     add_column    :contracts, :io_path, :string, :encode => :lzo, :limit => 512
     rename_column :contracts, :caf_path, :caf_path_old
     add_column    :contracts, :caf_path, :string, :encode => :lzo, :limit => 512
