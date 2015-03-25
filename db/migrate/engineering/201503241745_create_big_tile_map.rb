2a25fce566abdf928588f90af1dd560ca7bb20d8
Selene Tan <selenet@telenav.com>
2015-03-24 18:50:48 -0700

    Create BigTileMap tables on avails/eng
    - https://thinkneardev.atlassian.net/browse/GARD-1694

---
+++ db/migrate/engineering/201503241745_create_big_tile_map.rb
@@ -0,0 +1,20 @@
+class CreateBigTileMap < ActiveRecord::Migration
+  def up
+    create_table :big_tile_map, :id => false, :options => 'SORTKEY (tile_id)' do |t|
+      t.integer :tile_id, :null => false, :encode => :delta
+      t.integer :normalized_lat, :encode => :lzo
+      t.integer :normalized_lon, :encode => :lzo
+      t.string :state_id, :encode => :bytedict
+      t.string :zip, :encode => :lzo
+      t.string :dma, :encode => :bytedict
+      t.string :city_geoid, :encode => :lzo
+      t.string :timezone, :encode => :bytedict
+      t.integer :nielsen_dma, :encode => :bytedict
+    end
+    grant_select :big_tile_map
+  end
+
+  def down
+    drop_table :big_tile_map
+  end
+end
