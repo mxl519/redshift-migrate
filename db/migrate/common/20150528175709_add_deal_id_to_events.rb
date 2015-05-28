2204ab8621dc1f1677a938d56278598344710758
Vis <marcv@marcv-mbr.telenav.com>
2015-05-28 11:38:56 -0700

    Added deal_id and deal_ids to respective events tables
    https://thinkneardev.atlassian.net/browse/REX-3746

---
+++ db/migrate/common/20150528175709_add_deal_id_to_events.rb
@@ -0,0 +1,13 @@
+class AddDealIdToEvents < ActiveRecord::Migration
+  def change
+    add_column :auctions, :deal_ids, :string, :limit => 512, :encode => :lzo, :partitioned => :weekly
+    add_column :bids, :deal_id, :string, :encode => :lzo, :limit => 100, :partitioned => :weekly
+    add_column :bids_raw, :deal_id, :string, :encode => :lzo, :limit => 100, :partitioned => :hourly
+    add_column :clicks, :deal_id, :string, :encode => :lzo, :limit => 100, :partitioned => :monthly
+    add_column :conversions, :deal_id, :string, :encode => :lzo, :limit => 100, :partitioned => :monthly
+    add_column :impressions, :deal_id, :string, :encode => :lzo, :limit => 100, :partitioned => :monthly
+    add_column :impressions_final, :deal_id, :string, :encode => :lzo, :limit => 100, :partitioned => :monthly
+    add_column :tracking, :deal_id, :string, :encode => :lzo, :limit => 100, :partitioned => :monthly
+    add_column :win_notices, :deal_id, :string, :encode => :lzo, :limit => 100, :partitioned => :monthly
+  end
+end
