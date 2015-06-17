ea3af488a91fbb714a61c9a9f856b7056122b9aa
Vis <marcv@marcv-mbr.telenav.com>
2015-06-16 17:12:29 -0700

    Revised brandsafe_scores column type to string
    https://thinkneardev.atlassian.net/browse/REX-4186

--- db/migrate/common/20150616171320_add_brandsafe_scores_to_targeting_profile.rb
+++ db/migrate/common/20150616171320_add_brandsafe_scores_to_targeting_profile.rb
@@ -1,5 +1,5 @@
 class AddBrandsafeScoresToTargetingProfile < ActiveRecord::Migration
   def change
-    add_column :targeting_profiles, :brandsafe_scores, :text, :encode => :text255
+    add_column :targeting_profiles, :brandsafe_scores, :string, :encode => :text255
   end
 end
