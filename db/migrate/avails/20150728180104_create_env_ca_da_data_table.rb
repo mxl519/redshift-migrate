b22991bbdc19088180058c6d78797585def08d5b
Sergey <sergeyk@thinknear.com>
2015-07-28 11:19:10 -0700

    Adding new demographic data tables for Dissemination Areas (Canada)
    https://thinkneardev.atlassian.net/browse/GARD-2356

---
+++ db/migrate/avails/20150728180104_create_env_ca_da_data_table.rb
@@ -0,0 +1,14 @@
+class CreateEnvCaDaDataTable < ActiveRecord::Migration
+  def up
+    create_table :env_ca_da_data, :id => false, :options => 'SORTKEY (id, attribute_name)' do |t|
+      t.string :id, :limit => 25, :null => false, :encode => :lzo
+      t.string :attribute_name, :limit => 12, :null => false, :encode => :lzo
+      t.decimal :value, :precision => 11, :scale => 2, :encode => :mostly32
+    end
+    grant_select :env_ca_da_data
+  end
+
+  def down
+    drop_table :env_ca_da_data
+  end
+end
