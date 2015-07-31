75bf463a5aee7a4aa5f667464da1d3d423f47887
Sergey <sergeyk@thinknear.com>
2015-07-31 16:12:44 -0700

    Reducing decimal precision to 10+1 digits instead of 9+2
    https://thinkneardev.atlassian.net/browse/GARD-2356

--- db/migrate/avails/20150728180104_create_env_ca_da_data_table.rb
+++ db/migrate/avails/20150728180104_create_env_ca_da_data_table.rb
@@ -3,7 +3,7 @@ class CreateEnvCaDaDataTable < ActiveRecord::Migration
     create_table :env_ca_da_data, :id => false, :options => 'SORTKEY (id, attribute_name)' do |t|
       t.string :id, :limit => 25, :null => false, :encode => :lzo
       t.string :attribute_name, :limit => 12, :null => false, :encode => :lzo
-      t.decimal :value, :precision => 11, :scale => 2, :encode => :mostly32
+      t.decimal :value, :precision => 11, :scale => 1, :encode => :mostly32
     end
     grant_select :env_ca_da_data
   end
