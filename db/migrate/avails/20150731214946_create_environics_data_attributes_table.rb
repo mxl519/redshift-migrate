e878fb476a8a3f618250f97c14716d2c2032dd3d
Sergey <sergeyk@thinknear.com>
2015-08-04 12:12:14 -0700

    Removing sortkey from environics_data_attributes table
    https://thinkneardev.atlassian.net/browse/GARD-2362

--- db/migrate/avails/20150731214946_create_environics_data_attributes_table.rb
+++ db/migrate/avails/20150731214946_create_environics_data_attributes_table.rb
@@ -1,6 +1,6 @@
 class CreateEnvironicsDataAttributesTable < ActiveRecord::Migration
   def up
-    create_table :environics_data_attributes, :id => false, :options => 'SORTKEY (id, attribute_name)' do |t|
+    create_table :environics_data_attributes, :id => false do |t|
       t.string :attribute_name, :limit => 12, :null => false, :encode => :lzo
       t.string :description, :limit => 200, :null => false, :encode => :lzo
       t.string :source, :limit => 25, :null => false, :encode => :lzo
